2bc95bff46eed88bec0857e4c0d36dec7b2814b3
jonichi <jonichi16@gmail.com>
2025-05-04 09:42:18 +0800

    rename

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,6 @@
 import { type RouteConfig, index, prefix } from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.tsx"),
-  ...prefix("pokemons", [index("routes/pokemons/list.tsx")]),
+  index("routes/index.tsx"),
+  ...prefix("pokemons", [index("routes/pokemons/index.tsx")]),
 ] satisfies RouteConfig;
