cec2246ae36cfccd97e5531a292397f73885469f
jonichi <jonichi16@gmail.com>
2025-05-04 19:04:42 +0800

    test

--- app/features/pokemon/pages/PokemonList/index.tsx
+++ app/features/pokemon/pages/PokemonList/index.tsx
@@ -25,9 +25,9 @@ export default function PokemonList({
         <div className="space-y-6">
           <ul className="list-disc pl-5 space-y-1">
             {pokemons.map((p) => (
-              <Link key={p.name} to={`/pokemons/${p.name}`}>
-                <li className="capitalize text-lg">{p.name}</li>
-              </Link>
+              <li key={p.name} className="capitalize text-lg">
+                <Link to={`/pokemons/${p.name}`}>{p.name}</Link>
+              </li>
             ))}
           </ul>
           <div className="flex gap-4">
