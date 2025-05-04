6130d029aaafa0c2548b68b2ea89b094a9cf32c0
jonichi <jonichi16@gmail.com>
2025-05-04 16:09:08 +0800

    style

--- app/features/pokemon/pages/PokemonDetails/index.tsx
+++ app/features/pokemon/pages/PokemonDetails/index.tsx
@@ -9,15 +9,23 @@ export default function PokemonDetails({ pokemon }: PokemonDetailsProps) {
   const navigate = useNavigate();
 
   return (
-    <main>
-      <h1>Pokemon Details</h1>
-      <p>
-        Name: <span className="capitalize">{name}</span>
-      </p>
-      <img src={sprite} alt={`${name} sprite`} />
-      <button type="button" onClick={() => navigate(-1)}>
-        Back
-      </button>
+    <main className="min-h-screen p-5">
+      <div className="min-h-screen flex flex-col gap-5">
+        <h1 className="text-3xl font-bold">Pokemon Details</h1>
+        <p className="font-bold">
+          Name: <span className="capitalize">{name}</span>
+        </p>
+        <img className="w-[150px]" src={sprite} alt={`${name} sprite`} />
+        <div>
+          <button
+            className="bg-gray-200 hover:bg-gray-300 text-gray-800 font-medium py-2 px-4 rounded-lg shadow"
+            type="button"
+            onClick={() => navigate(-1)}
+          >
+            Back
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
